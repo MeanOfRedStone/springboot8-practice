0b2a61a105fffd1b7db093fa2ca48208e2eadabf
SeokEui Hong <hongs7701@naver.com>
2023-08-28 17:34:57 +0900

    #5 (1) 머스태치 예제 (2) 머스태치 예제 Testcode 작성

---
+++ src/main/java/org/redStone/springboot/web/dto/IndexController.java
@@ -0,0 +1,14 @@
+package org.redStone.springboot.web.dto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    //머스테치 스타터가 있기때문에 return에 알아서 머스테치 파일 경로가 붙는다.
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/org/redStone/springboot/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package org.redStone.springboot.web;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    //@Autowired 스프링이 관리하는 beans 주입받는다
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //전체 코드 다 검사할 것이 아니라 일부 코드가 있는 확인
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+
+}
